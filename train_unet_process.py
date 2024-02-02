42567aa89487e49904ade42bcbed58565b2a0b42
Ludovic Barusseau <ludovic.barusseau@ikomia.com>
2024-02-02 11:19:20 +0100

    chore: prepare Python 3.11 compatibility

--- train_unet_process.py
+++ train_unet_process.py
@@ -198,7 +198,7 @@ class TrainUnetFactory(dataprocess.CTaskFactory):
         # relative path -> as displayed in Ikomia application process tree
         self.info.path = "Plugins/Python/Segmentation"
         self.info.icon_path = "icon/unet.jpg"
-        self.info.version = "1.0.0"
+        self.info.version = "1.0.1"
         self.info.license = "GPL-3.0 license"
         # self.info.icon_path = "your path to a specific icon"
         self.info.authors = "Olaf Ronneberger, Philipp Fischer, Thomas Brox"
